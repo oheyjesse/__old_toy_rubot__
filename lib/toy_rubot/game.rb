0d3a31fd4c17d276054d11b48ff49b241f9003ae
oheydrew <drew@oheydrew.me>
2018-09-13 19:08:07 +1000

    Refactor create_table

--- lib/toy_rubot/game.rb
+++ lib/toy_rubot/game.rb
@@ -7,17 +7,14 @@ module ToyRubot
       @table = nil
     end
 
-    def create_table(size = nil)
-      @table = if size.nil?
-                 Table.new
-               else
-                 Table.new(size)
-               end
+    # args: {x: x, y: y} (optional)
+    def create_table(**args)
+      @table = Table.new(args)
     end
 
     def create_robot(x:, y:, facing:, **args)
       raise 'table must be created before robot' unless @table
-      raise 'robot not within bounds of table' unless @table.in_bounds?(x: x, y: y)
+      raise 'robot not in bounds of table' unless @table.in_bounds?(x: x, y: y)
 
       @robot = Robot.new(x: x, y: y, facing: facing, **args)
     end
